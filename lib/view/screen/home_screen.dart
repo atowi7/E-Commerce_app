a0f3536b3dcf96c988bc9f7bfd5d70225e45b126
atowi7 <dhoom.not@gmail.com>
2023-02-10 16:58:05 +0200

    Home page modifications

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/controller/home_controller.dart';
+import 'package:ecommerce_app/core/constant/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,13 +8,63 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    HomeController homeController = Get.put(HomeController());
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Text(homeController.id!),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[700],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[600],
+                      prefixIcon: const Icon(Icons.search_outlined),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: Colors.grey[700],
+                    borderRadius: BorderRadius.circular(20)),
+                child: const IconButton(
+                    iconSize: 30,
+                    onPressed: null,
+                    icon: Icon(Icons.notifications_active_outlined)),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            decoration: BoxDecoration(
+              color: AppColor.blue,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                const Text(
+                  'Discount Summer',
+                  style: TextStyle(fontSize: 30),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
